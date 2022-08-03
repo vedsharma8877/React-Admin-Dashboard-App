b19c3bf8956d8b7ba26f70d3636cd900203110ed
Ved Sharma <ved.sharma@SIGMAINFOJDR.COM>
2022-08-03 10:42:01 +0530

    color picker palette

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -6,6 +6,8 @@ const change = (args) => {
   document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
 }
 
+const CustomColorPicker = ({ id, mode }) => <ColorPickerComponent id={id} mode={mode} modeSwitcher={false} inline showButtons={false} change={change} />;
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -15,24 +17,11 @@ const ColorPicker = () => {
         <div className="flex justify-center items-center gap-20 flex-wrap">
           <div>
             <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
-            <ColorPickerComponent
-              id="inline-pallete"
-              mode="Palete"
-              modeSwitcher={false}
-              inline
-              showButtons={false}
-              change={change}
-            />
+            <CustomColorPicker id="inline-palette" mode="Palette" />
           </div>
           <div>
             <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
-            <ColorPickerComponent
-              id="inline-picker"
-              mode="Picker"
-              modeSwitcher={false}
-              inline
-              showButtons={false}
-            />
+            <CustomColorPicker id="inline-picker" mode="Picker" />
           </div>
         </div>
       </div>
